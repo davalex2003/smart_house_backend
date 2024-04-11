78fc702dc244c035377ab466ebd2fe56e4dc5e83
davalex2003 <pupkinvasek200356@gmail.com>
2024-04-11 20:57:47 +0300

    beauty

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,30 +1,32 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from picamera import PiCamera 
+from picamera import PiCamera
 import os
 import requests
 
 Movement_detected = False
-camera = PiCamera(resolution=(800,600))
-camera.shutter_speed = 60000 
+camera = PiCamera(resolution=(800, 600))
+camera.shutter_speed = 60000
 camera.iso = 200
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN)
 
-def DetectedCallback(channel):
+
+def detected_callback():
     global Movement_detected
     Movement_detected = True
-    print("Movement detected!") 
-    
-GPIO.add_event_detect(7, GPIO.RISING, callback=DetectedCallback)
+    print("Movement detected!")
 
-url="http://192.168.1.19:8000/security/raspberry"
+
+GPIO.add_event_detect(7, GPIO.RISING, callback=detected_callback)
+
+url = "http://192.168.1.19:8000/security/raspberry"
 
 try:
     while True:
-        if Movement_detected:            
+        if Movement_detected:
             try:
                 filename = os.path.join('192.168.1.9.png')
                 camera.capture(filename)
@@ -32,7 +34,7 @@ try:
                 requests.post(url, files=files)
             except Exception as E:
                 print(f'** Error {E} **')
-                pass       
+                pass
             Movement_detected = False
         sleep(0.1)
 except KeyboardInterrupt:
